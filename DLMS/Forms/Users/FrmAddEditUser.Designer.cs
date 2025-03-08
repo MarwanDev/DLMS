bc8614a7230884773fdbb764515489628b39dba3
marwandev <marwaneg@aucegypt.edu>
2025-03-08 14:52:02 +0200

    Make the save button in the user form disabled by default

--- DLMS/Forms/Users/FrmAddEditUser.Designer.cs
+++ DLMS/Forms/Users/FrmAddEditUser.Designer.cs
@@ -293,6 +293,7 @@
             // 
             // btnSave
             // 
+            this.btnSave.Enabled = false;
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSave.Location = new System.Drawing.Point(709, 718);
             this.btnSave.Name = "btnSave";
