b04a2211c8a166e8f5fba81c44dbc2b8e0fbe3d2
marwandev <marwaneg@aucegypt.edu>
2025-03-31 22:44:14 +0200

    Add the international licences history

--- DLMS/Forms/Licence/FrmLicenceHistory.cs
+++ DLMS/Forms/Licence/FrmLicenceHistory.cs
@@ -17,6 +17,7 @@ namespace DLMS.Forms.Licence
             InitializeComponent();
             CurrentPerson = person;
             GetAllLocalLicencesInDGV();
+            GetAllInternationalLicencesInDGV();
             Utils.ChangeUcPersonInfoControllersUI(ucPersonInfo2, person);
         }
 
@@ -33,13 +34,24 @@ namespace DLMS.Forms.Licence
             dgvLocalLicences.DataSource = localLicences;
             Utils.DisableDGVColumnSorting(dgvLocalLicences);
             dgvLocalLicences.Refresh();
-            UpdateLocalCountLabel();
+            UpdateCountLabels();
         }
 
-        private void UpdateLocalCountLabel()
+        private void GetAllInternationalLicencesInDGV()
+        {
+            DataTable localLicences = LicenceModel.GetAllInternationalLicencesPerPerson(CurrentPerson.ID);
+            dgvInternationalLicences.DataSource = localLicences;
+            Utils.DisableDGVColumnSorting(dgvInternationalLicences);
+            dgvInternationalLicences.Refresh();
+            UpdateCountLabels();
+        }
+
+        private void UpdateCountLabels()
         {
             lblLocalCount.Text = LicenceModel.GetAllLocalLicencePerPersonCount(CurrentPerson.ID).ToString();
+            lblInternationalCount.Text = LicenceModel.GetAllInternationalLicencePerPersonCount(CurrentPerson.ID).ToString();
             lblLocalCount.Refresh();
+            lblInternationalCount.Refresh();
         }
 
         private void FrmLicenceHistory_FormClosed(object sender, FormClosedEventArgs e)
